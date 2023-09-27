6902ed9cd96c373bc92145530daa5fa6938c8f05
Cale Watkins <caleawatkins@gmail.com>
2023-09-26 20:05:20 -0400

    add recipe and shoppong list components.  Update html templates

---
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is for testing', 'https://clipground.com/images/clipart-food-photos.jpg'),
+    new Recipe('Test Recipe 2', 'This is for testing', 'https://clipground.com/images/clipart-food-photos.jpg')
+  ];
+}
