1798c6338cdff25265e08af9a51529a304cb3556
Cale Watkins <caleawatkins@gmail.com>
2023-10-08 16:44:01 -0400

    add directive to details and header html to allow dropdown

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+  constructor(private elRef: ElementRef) {}
 
-  constructor() { }
+  //constructor(private el: ElementRef, private renderer: Renderer2) { }
 
+  // @HostListener('click') onClick() {
+  //   const nativeElement = this.el.nativeElement;
+
+  //   if(!Array.from(nativeElement.classList).includes('open')) {
+  //     this.renderer.addClass(nativeElement, 'open');
+  //   } else {
+  //     this.renderer.removeClass(nativeElement, 'open');
+  //   }
+
+  //}
 }
